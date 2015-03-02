a6646352e349d38698bb973678cc15e4d3124688
Chrisg737 <chrisg93@gmail.com>
2015-03-01 21:16:18 -0500

    Update CardProject.cpp
    Created using visual studio express 2014 so you need the stdafx.h header file

--- CardProject.cpp
+++ CardProject.cpp
@@ -9,7 +9,9 @@
 #include <sstream>
 #include <ctime>
 
-using namespace std;
+using namespace std;  
+
+stack <float> main_deck;    //Using a global stack so we can push our array of cards into the stack
 
 string translater(float cardnumber) //takes in card number and returns the name of card
 {
@@ -63,16 +65,8 @@ string translater(float cardnumber) //takes in card number and returns the name 
 }
 
 
-int main()
+void shuffle() //Deck Shuffling Function
 {
-	float players; //number of players
-	cout << "Welcome to the Job Card Game!" << endl;
-	cout << "---------------------------------------\n";
-	cout << "Rules: The game allows up to 4 players. 56 cards in a deck with 4 penalty cards\n" << endl;
-	cout << "The deck is shuffled every round and each card is ranked based on face value and secondly their suit. Penalty cards are not ranked\n" << endl;
-	cout << "At the end of each round the player with the highest rank card is the winner and their score is increased by 2. Players with penalty cards have their scores decreased by 2.\n" << endl;
-	//The 2nd to 10th cards are the number cards, the next 3 are king queen and jack. The first card is the ace. These are the cards per suite in a unshuffled deck. The 53-56 cards are penalty cards.
-	//Card number one would be ace of clubs. number 14 is ace of diamonds. Number 27 is ace of hearts. Number 40 is ace of diamonds.  In this implementation, ace is lowest rank.
 	float array_cards[56]; //Array has deck of cards.
 	float temp_deck[56];
 	int j;
@@ -83,18 +77,6 @@ int main()
 	for (int i = 0; i < 56; i++)  //this loop places the cards into the main array or deck
 	{
 		array_cards[i] = i + 1;
-	}
-
-	cout << "Please enter the number of players for this game: \n";
-	cin >> players;
-	while (players < 2 || players > 4)
-	{
-		cout << "You need more than 1 player to play the game\n";
-		cout << "pick more than 1 person but less than 5 people.\n";
-		cout << "Please enter the number of players for this game: \n";
-		cin >> players;
-		if (players >= 2 && players <= 4)
-			break;
 	}
 	srand(time(NULL));
 	for (int i = 0; i < 56; i++)   //shuffle deck
@@ -110,37 +92,67 @@ int main()
 		}
 	}
 
-	stack <float> main_deck;            //Using a stack so we can push our array of cards into the stack
 	for (int i = 0; i < 56; i++)
 	{
 		main_deck.push(temp_deck[i]);
 	}
 
+}
 
+
+int main()
+{
+	float players; //number of players
+	cout << "Welcome to the Job Card Game!" << endl;
+	cout << "---------------------------------------\n";
+	cout << "Rules: The game allows up to 4 players. 56 cards in a deck with 4 penalty cards\n" << endl;
+	cout << "The deck is shuffled every round and each card is ranked based on face value and secondly their suit. Penalty cards are not ranked\n" << endl;
+	cout << "At the end of each round the player with the highest rank card is the winner and their score is increased by 2. Players with penalty cards have their scores decreased by 2.\n" << endl;
+	cout << "To draw a card, press enter" << endl;
+	//The 2nd to 10th cards are the number cards, the next 3 are king queen and jack. The first card is the ace. These are the cards per suite in a unshuffled deck. The 53-56 cards are penalty cards.
+	//Card number one would be ace of clubs. number 14 is ace of diamonds. Number 27 is ace of hearts. Number 40 is ace of diamonds.  In this implementation, ace is lowest rank.
+
+	cout << "Please enter the number of players for this game and click enter: \n";
+	cin >> players;
+	getchar();
+	while (players < 2 || players > 4)
+	{
+		cout << "You need more than 1 player to play the game\n";
+		cout << "pick more than 1 person but less than 5 people.\n";
+		cout << "Please enter the number of players for this game: \n";
+		cin >> players;
+		if (players >= 2 && players <= 4)
+			break;
+	}
+	shuffle();
 	//Deck is shuffled
 	int scores[4];
 	int pickedcards[4];
 	while (-1)  //infinite while loop
 	{
-
+		system("cls"); //Clears the console to look nicer each round.
 		cout << endl;
 		for (int i = 0; i < players; i++)
 		{
 
 			//round 1 starts and first person picks card
-			cout << "Player" << i + 1 << "'s card is " << translater(main_deck.top()) << endl;
-			pickedcards[i] = main_deck.top();
-			if (pickedcards[i] > 52)
-			{
-				scores[i] = scores[i] - 1;
-				pickedcards[i] = 0;
-			}
-			if (scores[i] < 0)
-			{
-				scores[i] = 0;
-			}
-			main_deck.pop();  //each card is seen and then popped/discarded.
+			//Press a button to pick a card and click enter
 
+				cout << "Player" << i + 1 << " click enter to pick a card." << endl;
+				getchar();
+				cout << "Player" << i + 1 << "'s card is " << translater(main_deck.top()) << endl;
+				pickedcards[i] = main_deck.top();
+				if (pickedcards[i] > 52)
+				{
+					scores[i] = scores[i] - 1;
+					pickedcards[i] = 0;
+				}
+				if (scores[i] < 0)
+				{
+					scores[i] = 0;
+				}
+				main_deck.pop();  //each card is seen and then popped/discarded.
+		
 		}
 		float max = 0;
 		for (int i = 0; i < players; i++)
@@ -164,13 +176,15 @@ int main()
 				cout << " " << scores[i] << "   ";
 			}
 			cout << endl;
-			if (scores[0] >= 21)
+			if ((scores[0] >= 21) && (scores[0] - scores[2] >= 2) && (scores[0] - scores[3] >= 2) && (scores[0] - scores[1] >= 2))
 			{
 				cout << endl;
 				cout << "Winner is player 1!" << endl;
 				break;
 			}
+			getchar();
 		}
+
 		else if (max == pickedcards[1])
 		{
 			cout << "Round winner is" << " Player " << 2 << endl;
@@ -183,12 +197,13 @@ int main()
 				cout << " " << scores[i] << "   ";
 			}
 			cout << endl;
-			if (scores[1] >= 21)
+			if ((scores[1] >= 21) && (scores[1] - scores[2] >= 2) && (scores[1] - scores[3] >= 2) && (scores[1] - scores[0] >= 2))
 			{
 				cout << endl;
 				cout << "Winner is player 2!" << endl;
 				break;
 			}
+			getchar();
 		}
 		else if (max == pickedcards[2])
 		{
@@ -202,12 +217,13 @@ int main()
 				cout << " " << scores[i] << "   ";
 			}
 			cout << endl;
-			if (scores[2] >= 21)
+			if ((scores[2] >= 21) && (scores[2] - scores[3] >= 2) && (scores[2] - scores[1] >= 2) && (scores[2] - scores[0] >= 2))
 			{
 				cout << endl;
 				cout << "Winner is player 3!" << endl;
 				break;
 			}
+			getchar();
 		}
 		else if (max == pickedcards[3])
 		{
@@ -221,15 +237,21 @@ int main()
 				cout << " " << scores[i] << "   ";
 			}
 			cout << endl;
-			if ((scores[3] >= 21))
+			if ((scores[3] >= 21) && (scores[3] - scores[2] >= 2) && (scores[3] - scores[1] >= 2) && (scores[3] - scores[0] >= 2))
 			{
 				cout << endl;
 				cout << "Winner is player 4!" << endl;
 				break;
 			}
+			getchar();
 		}
+		while (main_deck.empty() == false)
+		{
+			main_deck.pop();
+		}
+		shuffle();  //Shuffles after each round.
 
 	}
-	int x;
-	cin >> x;
+	int x;   //press an integer and enter to exit
+	cin >> x; 
 }
